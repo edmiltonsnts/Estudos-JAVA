e55fd0f298136a8ad1835b9616f0f46175adcba7
Edmilton Santos <EDMILTONSNTS@GMAIL.COM>
2024-07-18 15:44:42 -0300

    add new

---
+++ src/BoletimEstudante.java
@@ -0,0 +1,14 @@
+public class BoletimEstudante {
+
+  public static void main(String[] args) {
+    int mediaFinal = 8;
+    if (mediaFinal<6) 
+    System.out.println("Reprovado");
+    else if (mediaFinal ==6)
+    System.out.println("Recuperação");
+    else
+    System.out.println("Aprovado");
+  
+    
+  }
+}
