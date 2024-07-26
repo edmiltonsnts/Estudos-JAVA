e45ec360f63b9a274994752694f4229c0123f593
Edmilton Santos <EDMILTONSNTS@GMAIL.COM>
2024-07-26 16:46:41 -0300

    New arq

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        boolean fimDeSemana = false;
+        boolean fazendoSol = false;
+        boolean vamosApraia = fimDeSemana && fazendoSol;
+
+        //Tabela Verdade
+        //Operador && (AND)
+        // true && true = true
+        // true && false = false
+        // false && true  = false
+        // false && false = false
+
+
+        // Operador || (OR)
+        // true && true = true
+        // true && false = true
+        // false && true  = true
+        // false && false = false
+
+
+        System.out.println(vamosApraia);
+
+        String mensagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
+        System.out.println(mensagem);
+
     }
 }
--- src/tipos-variaveis/src/TipoDados.java
+++ src/tipos-variaveis/src/TipoDados.java
@@ -1,10 +1,18 @@
 public class TipoDados {
 	public static void main(String[] args) {
-		byte idade = 123;
+		byte idade = 127;
 		short ano = 2021;
-		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
-		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		int cep = 21070333; 
+		long cpf = 98765432109L; 
 		float pi = 3.14F;
 		double salario = 1275.33;
+
+
+		System.out.println(idade);
+		System.out.println(ano);
+		System.out.println(cep);
+		System.out.println(cpf);
+		System.out.println(pi);
+		System.out.println(salario);
 	}
 }
