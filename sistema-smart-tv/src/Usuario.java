b871473b59f27121f5341ae29d69a16099e396bf
Edmilton Santos <EDMILTONSNTS@GMAIL.COM>
2024-08-01 11:14:06 -0300

    add modification

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,15 +1,34 @@
 public class Usuario {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
 
          SmarTv smarTv  = new SmarTv();
 
+         smarTv.diminuirVolume();
+         smarTv.diminuirVolume();
+         smarTv.diminuirVolume();
+         smarTv.aumentarVolume();
+
+         System.out.println("Canal Atual : " + smarTv.canal);
+
+         smarTv.mudarCanal(13); 
+         
+         System.out.println("Canal Atual : " + smarTv.canal);
+
+         System.out.println("Volume Atual : " +smarTv.volume);
+
 
          System.out.println("TV Ligada ? "  + smarTv.ligada);
-         System.out.println("Canal Atual : " + smarTv.canal);
+         
          System.out.println("Volume Atual : " + smarTv.volume);
+
 
          smarTv.ligar ();
          System.out.println("Novo Status -> TV Ligada ? "  + smarTv.ligada);
+
 
          smarTv.desligar();
          System.out.println("Novo Status -> TV Ligada ? "  + smarTv.ligada);
