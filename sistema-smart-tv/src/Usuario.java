81904f4b36b79440e688e63eb45dfcf560bfa4f1
Edmilton Santos <EDMILTONSNTS@GMAIL.COM>
2024-08-01 10:36:58 -0300

    add new files

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,14 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+         SmarTV smartv  = new SmarTV();
+
+
+         System.out.println("TV Ligada ? "  + smartv.ligada);
+         System.out.println("Canal Atual : " + smartv.canal);
+         System.out.println("Volume Atual : " + smartv.volume);
+
+         
+        
+    }
+}
