500720f7508124105c2859c218f493019bbfa3e0
Edmilton Santos <EDMILTONSNTS@GMAIL.COM>
2024-08-28 16:22:48 -0300

    feat new files

---
+++ Estruturas/EstruturasDeRepeticao/ExemploForArrays.java
@@ -0,0 +1,7 @@
+public static void main(String[] args) {
+ String alunos [] = {"FELIPE", "JONAS", "JOSE", "JULIA", "MARIA"};
+
+  for(String aluno : alunos) {
+    System.out.println("Nome do aluno é: " + aluno);
+  }
+}
